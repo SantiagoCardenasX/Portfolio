86d39aff695405b6f7cd692aba48673ab3eeda18
SantiagoCardenasX <santiago.cardenasdelgadillo@edu.sait.ca>
2024-09-25 01:09:36 -0600

    Initial prototype

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+menuToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
