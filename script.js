83a6475384e69ba5bd2ba2b7ea164155d6a2ecb4
SantiagoCardenasX <santiago.cardenasdelgadillo@edu.sait.ca>
2024-09-25 21:12:43 -0600

    first version finished

--- script.js
+++ script.js
@@ -38,4 +38,9 @@ document.addEventListener("DOMContentLoaded", function() {
       observer.observe(section);
     });
   });
+
+//   Tooltips
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
   
