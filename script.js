3be626623e49bcc753e682ab81006cf3ff81ec40
SantiagoCardenasX <santiago.cardenasdelgadillo@edu.sait.ca>
2024-09-25 18:48:24 -0600

    Skills added, sidebar fixed and added features, fixed bugs

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+// Toggles the 'active' class on the sidebar when the menu toggle button is clicked.
+
 const menuToggle = document.getElementById('menu-toggle');
 const sidebar = document.querySelector('.sidebar');
 
 menuToggle.addEventListener('click', () => {
     sidebar.classList.toggle('active');
 });
+
+// Scrollspy for the Navbar
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('#sections .nav-link');
+  
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.6 
+    };
+  
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const id = entry.target.getAttribute('id');
+          navLinks.forEach(link => {
+            if (link.getAttribute('href') === `#${id}`) {
+              link.classList.add('active');
+            } else {
+              link.classList.remove('active');
+            }
+          });
+        }
+      });
+    }, observerOptions);
+  
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+  });
+  
