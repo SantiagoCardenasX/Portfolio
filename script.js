8ed96e144dfec75ab4f2da3545727b6ad42f804c
SantiagoCardenasX <santiagocardenass@hotmail.com>
2025-02-01 03:22:09 -0700

    Mobile animation fixed

--- script.js
+++ script.js
@@ -54,13 +54,13 @@ const text = document.querySelector(".second-text");
 
 const textLoad = () => {
   setTimeout(() => {
-    text.textContent = "Software Developer";
+    text.textContent = "Developer";
   }, 0);
   setTimeout(() => {
     text.textContent = "Freelancer";
   }, 4000);
   setTimeout(() => {
-    text.textContent = "Problem Solver";
+    text.textContent = "Visionary";
   }, 8000);
 };
 
